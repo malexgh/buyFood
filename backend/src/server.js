11b0e92e6f2700c22c64ee5f3550e2f09ebddc32
Marcio <alexandrino.marcio@gmail.com>
2019-12-17 14:27:47 -0300

    adding mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/buyFood',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
 const app = express();
 
 app.get('/', function (req, res) {
