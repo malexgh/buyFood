c98baf11f72aaf43956c6deca38d054695a70411
Marcio <alexandrino.marcio@gmail.com>
2019-12-26 13:40:35 -0300

    adding users routes

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,13 +2,33 @@ const User = require('../models/user');
 
 module.exports = {
     async store(req, res) {
-        const user = new User(req.body);
         try {
+            const user = new User(req.body);
             await user.save();
             const token = await user.generateAuthToken();
             res.status(201).send({ user, token });
         } catch (error) {
-            res.status(400).send({error});
+            res.status(500).send({ error });
+        }
+    },
+    async index(req, res) {
+        try {
+            const users = await User.find();
+            res.status(200).send({ users });
+        } catch (error) {
+            res.status(500).send({ error });
+        }
+    },
+    async show(req, res) {
+        try {
+            const user = await User.findById(req.params.id);
+            if (user) {
+                res.status(200).send({ user });
+            } else {
+                res.status(400).send({ message: 'User not found' });
+            }
+        } catch (error) {
+            res.status(500).send({ error });
         }
     },
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('./controllers/userController');
 
 router.post('/users', userController.store);
+router.get('/users', userController.index);
+router.get('/users/:id', userController.show);
 
 module.exports = router;
