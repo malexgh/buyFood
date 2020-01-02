3e449d74592a38d8886477cd6fb5e9af4a8f13cf
Marcio <alexandrino.marcio@gmail.com>
2020-01-02 18:14:27 -0300

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Login from './pages/Login';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login></Login>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './Login.scss';
+import logo from '../logo.svg';
+
+const baseUrl = 'http://localhost:3333';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [remember, setRemember] = useState(true);
+
+  async function handleLogin(event) {
+    setError('');
+    const response = await fetch(`${baseUrl}/sessions/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
+    //console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data.token);
+    }
+    else {
+      setError('Login failed');
+    }
+  }
+
+  return (
+    <div className="Login">
+      <header className="Login-header">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo"></img>
+        </div>
+        <label htmlFor="email"><b>Email</b></label>
+        <input className="input-email"
+          name="email"
+          value={email}
+          placeholder="Enter Email"
+          onChange={(event) => setEmail(event.target.value)}>
+        </input>
+        <label htmlFor="password"><b>Password</b></label>
+        <input className="input-password"
+          name="password"
+          value={password}
+          placeholder="Enter Password"
+          onChange={(event) => setPassword(event.target.value)}>
+        </input>
+        <button className="button-login" onClick={handleLogin}>
+          Login
+        </button>
+        <label>
+          <input type="checkbox"
+            name="remember"
+            checked={remember}
+            onChange={(event) => setRemember(event.target.checked)}>
+          </input>
+          Remember me
+        </label>
+      </header>
+      {error && <footer className="Login-footer">
+        <p>{error}</p>
+      </footer>}
+    </div>
+  );
+}
+
+export default Login;
